7e14816d2c34af3f768aec3e5441afa68e3ba00e
shenyifan <605729908@qq.com>
2021-07-28 17:31:18 +0800

    up

--- src/views/Shanghai.vue
+++ src/views/Shanghai.vue
@@ -35,6 +35,9 @@ export default {
       this.scene.add(gridHelper);
       this.initLight(1.2);
       this.initCamera();
+      this.groundBehind();
+      this.groundFront();
+      this.tower();
       this.initRender();
     },
     initLight(intensity) {
@@ -73,6 +76,108 @@ export default {
       this.controls.addEventListener("change", () => {
         this.renderer.render(this.scene, this.camera);
       }); // 监听鼠标、键盘事件
+    },
+    groundBehind() {
+      // 地面2 后半部分
+      let shape = new THREE.Shape();
+      shape.moveTo(45, 100); // moveTo( x, y )
+      shape.lineTo(50, 100); // lineTo( x, y ) - 线
+      shape.lineTo(50, 0); // lineTo( x, y ) - 线
+      shape.lineTo(-50, 0); // lineTo( x, y ) - 线
+      shape.lineTo(-50, 60); // lineTo( x, y ) - 线
+      // 贝塞尔曲线
+      shape.bezierCurveTo(5, 15, 15, 5, 45, 100);
+
+      let extrudeGeometry = new THREE.ExtrudeGeometry(shape, {
+        depth: 3,
+        steps: 2,
+        bevelThickness: 0,
+        bevelSize: 1,
+      });
+
+      let material = new THREE.MeshLambertMaterial({ color: "gray" });
+
+      let mesh = new THREE.Mesh(extrudeGeometry, material);
+
+      mesh.receiveShadow = true;
+      mesh.rotation.x = Math.PI + Math.PI / 2; // 地面旋转180度
+      mesh.rotation.y = Math.PI; // 地面旋转180度
+
+      mesh.position.set(0, 0, 50);
+      this.scene.add(mesh);
+    },
+    groundFront() {
+      // 地面1 前半部分
+      let shape = new THREE.Shape();
+      shape.moveTo(50, 0); // moveTo( x, y )
+      shape.lineTo(-25, 0); // lineTo( x, y ) - 线
+      shape.quadraticCurveTo(-10, 107, 50, 15); // 二次曲线
+
+      let extrudeGeometry = new THREE.ExtrudeGeometry(shape, {
+        depth: 3,
+        steps: 2,
+        bevelThickness: 0,
+        bevelSize: 1,
+      });
+
+      let material = new THREE.MeshLambertMaterial({ color: "#666" });
+
+      let mesh = new THREE.Mesh(extrudeGeometry, material);
+
+      mesh.receiveShadow = true;
+      mesh.rotation.x = Math.PI / 2; // 地面旋转90度
+      mesh.position.set(0, 0, -50);
+      this.scene.add(mesh);
+    },
+    tower() {
+      // const self=this;
+      let tower = new THREE.Group();
+      let material = new THREE.MeshLambertMaterial({ color: "#666" });
+      let base = new THREE.Mesh(
+        new THREE.CylinderGeometry(10, 10, 1),
+        material
+      );
+      base.position.y = 0;
+      tower.add(base);
+      let base2 = new THREE.Mesh(new THREE.CylinderGeometry(8, 8, 1), material);
+      base2.position.y = 1;
+      tower.add(base2);
+
+      let pillar1 = new THREE.Mesh(
+        new THREE.CylinderGeometry(1, 1, 15),
+        material
+      );
+      pillar1.position.y = 9;
+      pillar1.position.x = 4;
+      pillar1.position.z = 0;
+      let pillar2 = pillar1.clone();
+      pillar2.position.y = 9;
+      pillar2.position.x = -Math.cos((Math.PI * 2) / 6) * 4;
+      pillar2.position.z = -Math.sin((Math.PI * 2) / 6) * 4;
+      let pillar3 = pillar1.clone();
+      pillar3.position.y = 9;
+      pillar3.position.x = -Math.cos((Math.PI * 2) / 6) * 4;
+      pillar3.position.z = Math.sin((Math.PI * 2) / 6) * 4;
+      tower.add(pillar1);
+      tower.add(pillar2);
+      tower.add(pillar3);
+      let ball1 = new THREE.Mesh(new THREE.SphereGeometry(7, 10, 10), material);
+      ball1.position.y = 20;
+      ball1.position.x = 0;
+      ball1.position.z = 0;
+      tower.add(ball1);
+      let pillar_1 = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.5, 0.5, 15),
+        material
+      );
+      let axis = new THREE.Vector3(0, 2, 0); // 向量axis
+      pillar_1.rotateOnAxis(axis, Math.PI / 8);
+
+      pillar_1.position.y = 20;
+      pillar_1.position.x = 20;
+      pillar_1.position.z = 0;
+      tower.add(pillar_1);
+      this.scene.add(tower);
     },
   },
 };
